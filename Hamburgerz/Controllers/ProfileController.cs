e61cb04902211e84ccaec9722ff8775abbd98a79
SamuelisGaming <samuolismatas@gmail.com>
2026-02-20 14:40:04 +0200

    Initial commit
    kzn pradzia ar kazka

---
+++ Hamburgerz/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hamburgerz.Controllers
+{
+	public class ProfileController : Controller
+	{
+		public IActionResult History()
+		{
+			return View();
+		}
+	}
+}
